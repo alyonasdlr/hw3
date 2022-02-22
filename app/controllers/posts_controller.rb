ececf5f5e8a9877890a41063a05dd5e8f668e3ff
alyonasdlr <elena.avdeeva@kellogg.northwestern.edu>
2022-02-22 02:10:20 +0000

    11

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+
+    def Create
+        @posts = Post.new(params["posts"])
+        @post.save
+        redirect_to "/places"
+       
+        
+       end
+
+
+
 end
