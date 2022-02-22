5eba72bb82fe17ffeb84d2e0b4d0f7c13931805e
alyonasdlr <elena.avdeeva@kellogg.northwestern.edu>
2022-02-22 05:27:20 +0000

    finished all the moves

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,19 @@
 class PostsController < ApplicationController
 
-    def Create
-        @posts = Post.new(params["posts"])
+    def show
+        @post = Post.find(params["id"])
+      end
+    
+      def new
+        @post = Post.new
+        @place = Place.find(params["place_id"])
+        @post.place_id = @place.id
+      end
+    
+      def create
+        @post = Post.new(params["post"])
         @post.save
-        redirect_to "/places"
-       
-        
-       end
-
-
-
-end
+        redirect_to "/places/#{@post.place.id}"
+      end
+    
+    end
